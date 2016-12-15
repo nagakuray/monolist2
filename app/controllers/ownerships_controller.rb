68de34ff37f20fe788fe88c3fad911fc26a4d860
yuichi <naruhodo.u1@icloud.com>
2016-12-15 07:15:44 +0000

    ボタンが切り替わらない

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -7,17 +7,16 @@ class OwnershipsController < ApplicationController
     else
       @item = Item.find(params[:item_id])
     end
-
+    
     # itemsテーブルに存在しない場合は楽天のデータを登録する。
     if @item.new_record?
-      # TODO 商品情報の取得 ::Ichiba:RakutenWebService:Item.search を用いてください
-      response = RakutenWebService::Ichiba::Item.search(
-        keyword: params[:q],
-        imageFlag: 1,
-      )
-      @items = response.first(20)
       
-      items = {}
+      # TODO 商品情報の取得 ::Ichiba:RakutenWebService:Item.search を用いてください
+      items = RakutenWebService::Ichiba::Item.search(
+        itemCode: params[:item_code],
+      )
+
+      #items = {}
 
       item                  = items.first
       @item.title           = item['itemName']
@@ -32,22 +31,35 @@ class OwnershipsController < ApplicationController
     # params[:type]の値にHaveボタンが押された時には「Have」,
     # Wantボタンが押された時には「Want」が設定されています。
     
-    # type = params[:type]
-    # if type == "Have" then
-    #   aaa="aaa"
-    # elsif type == "Want" then
-    #   aaa="aaa"
-    # end
+    unless current_user.have?
+      current_user.have(@item)
+      params[:type] = "Have it"
+    end
+    
+    unless current_user.want?
+      current_user.want(@item)
+      params[:type] = "Want it"
+    end
     
 
   end
 
   def destroy
     @item = Item.find(params[:item_id])
-
+    
     # TODO 紐付けの解除。 
     # params[:type]の値にHave itボタンが押された時には「Have」,
     # Want itボタンが押された時には「Want」が設定されています。
-
+    
+    if current_user.have?
+      current_user.unhave(@item)
+      params[:type] = "Have"
+    end
+    
+    if current_user.want?
+      current_user.unwant(@item)
+      params[:type] = "Want"
+    end
+    
   end
 end
