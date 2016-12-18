e9cc39ed28b05f51b97d3d2685eeb5fa65deeabc
nagakuray <naruhodo.u1@icloud.com>
2016-12-18 12:11:00 +0000

    ランキングの別の書き方をコミット

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -3,6 +3,10 @@ class RankingController < ApplicationController
     ranking = Have.group(:item_id).order('count_item_id desc').count('item_id').keys
     h_ranking = Item.find(ranking).map{ |model| [model.id, model] }.to_h
     @items = ranking.map{ |id| h_ranking[id] }.first(10)
+    
+    # 別の書き方    
+    # item_ids = Have.group(:item_id).order('count_item_id desc').limit(10).count('item_id').keys
+    # @items = Item.find(item_ids).sort_by{|item| item_ids.index(item.id)}
   end
 
   def want
